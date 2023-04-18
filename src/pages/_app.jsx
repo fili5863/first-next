4be69ed5a6db3438773e9029b27aabc87c444425
Jachob Westh <jachobwesth@gmail.com>
2023-04-18 13:23:14 +0200

    myapp

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
-import App from "next/app";
+import MyApp from "next/app";
 
 export default function MyApp({ Component, pageProps, navData }) {
   return (
